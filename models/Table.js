554fccca072ed472967b1809d516f1838c9f8dd7
Byungwook Yoon <byung90@gmail.com>
2021-05-11 17:38:52 -0400

    added tables model

---
+++ models/Table.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Table extends Model { }
+
+Table.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    min_bet: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'table'
+  }
+)
+
+module.exports = Table;
