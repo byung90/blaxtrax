c5cc61e69a6398656aa8c501947b949366218574
Taylor Leong <tjl2125@alum.barnard.edu>
2021-05-14 02:56:16 -0400

    finally got the table_id numbers to render on tableList

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Project, User, TablePlayer } = require("../models");
+const { Project, User, TablePlayer, Table } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
