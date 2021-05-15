1283c8ea80fc650a929d82cde92cb3adfa31a70e
Byungwook Yoon <byung90@gmail.com>
2021-05-15 07:18:22 -0400

    make sync

---
+++ controllers/api/deal.js
@@ -0,0 +1,159 @@
+const { Hand, Bet, Table, TablePlayer, User, Card } = require('../../models');
+const sequelize = require('../../config/connection');
+const { QueryTypes } = require('sequelize');
+
+const createHands = async (hands) => {
+  try {
+    const handData = await Hand.bulkCreate(hands);
+    return handData;
+  }
+  catch (err) {
+    return (err);
+  }
+}
+
+const getTable = async (id) => {
+  try {
+    const tableData = await Table.findByPk(id, {
+      include: [{ model: TablePlayer, include: [{ model: User, attributes: ['id', 'name', 'balance'] }] }]
+    })
+    return tableData;
+  }
+  catch (err) {
+    return (err);
+  }
+}
+
+const createBets = async (betAmounts) => {
+  try {
+    const betData = await Bet.bulkCreate(betAmounts, {
+      individualHooks: true,
+      returning: true,
+    });
+    return betData
+  }
+  catch (err) {
+    return (err);
+  }
+}
+
+const updateBalance = async (user) => {
+  try {
+    const updatedBalanceData = await User.update(user, {
+      where: {
+        id: user.id
+      },
+      fields: ['balance']
+    });
+    return betData
+  }
+  catch (err) {
+    return (err);
+  }
+}
+
+const getCard = async (card) => {
+  // Check and assign if randomCardIndex does not exist
+  try {
+    const cardData = await Card.create(card);
+    return cardData;
+  }
+  catch (err) {
+    return err;
+  }
+}
+
+// getUnqiueCard
+const getUniqueCard = async (tableId) => {
+  try {
+    // Check and assign if randomCardIndex does not exist
+    let randomCardIndex;
+    let doesRandomCardIndexExist = true;
+    while (doesRandomCardIndexExist) {
+      randomCardIndex = Math.floor(Math.random() * 52);
+      const checkRandomCardIndexExist = await sequelize.query("SELECT card_array_index FROM card JOIN hand ON card.hand_id = hand.id JOIN bet ON bet.id = hand.bet_id JOIN tablePlayer ON tablePlayer.id = bet.table_player_id JOIN `table` ON table.id = tablePlayer.table_id WHERE table.id=? and card.card_array_index=?", {
+        replacements: [tableId, randomCardIndex],
+        nest: false,
+        raw: true,
+        type: QueryTypes.SELECT
+      });
+      if (checkRandomCardIndexExist.length === 0) {
+        doesRandomCardIndexExist = false;
+      }
+    }
+    return randomCardIndex;
+  }
+  catch (err) {
+    return err
+  }
+}
+
+// check if deck can be continued
+const isDeckPlayable = async (tableId) => {
+  try {
+    const allUsedCardsinTable = await Card.count({
+      include: [
+        {
+          model: Hand, include: [
+            {
+              model: Bet, include: [
+                {
+                  model: TablePlayer, include: [
+                    {
+                      model: Table, where: {
+                        id: tableId
+                      }
+                    }
+                  ],
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    })
+
+    if (52 - allUsedCardsinTable < 10) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  catch (err) {
+    return err;
+  }
+}
+
+// Reset card deck
+const resetCard = async (tableId) => {
+  try {
+    const deleteBetsData = await sequelize.query("DELETE bet FROM bet JOIN tablePlayer ON bet.table_player_id = tablePlayer.id JOIN `table` ON tablePlayer.table_id = table.id where table.id=?", {
+      replacements: [tableId],
+      nest: false,
+      raw: true,
+      type: QueryTypes.DELETE
+    });
+    console.log(deleteBetsData);
+    if (deleteBetsData === null) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  catch (err) {
+    return (err);
+  }
+}
+
+module.exports = {
+  createHands,
+  getTable,
+  createBets,
+  getUniqueCard,
+  getCard,
+  isDeckPlayable,
+  updateBalance,
+  resetCard
+};
--- public/js/tableGame.js
+++ public/js/tableGame.js
@@ -47,74 +47,54 @@ const decreaseBet = () => {
 }
 
 const dealCards = async () => {
-  // Check if there are enough cards to play
-  const isPlayableData = await fetch('/api/gameRoutes/1/isPlayable', {
-    method: 'GET',
+  isInGame = true;
+  console.log(table_id);
+  const dealCardsData = await fetch('/api/gameRoutes/makeBets', {
+    method: 'POST',
+    body: JSON.stringify({
+      tableId: table_id,
+      tablePlayers: [
+        {
+          id: tablePlayers[0].id,
+          bet_amount: 0,
+          position: 0
+        },
+        {
+          id: tablePlayers[1].id,
+          bet_amount: bet_amount,
+          position: 1
+        }
+      ]
+    }),
     headers: { "Content-Type": "application/json" }
   });
 
-  if (isPlayableData.ok) {
-    const isPlayabe = await isPlayableData.json();
-    if (isPlayabe) {
-      isInGame = true;
-      console.log(table_id);
-      const dealCardsData = await fetch('/api/gameRoutes/makeBets', {
-        method: 'POST',
-        body: JSON.stringify({
-          tableId: table_id,
-          tablePlayers: [
-            {
-              id: tablePlayers[0].id,
-              bet_amount: 0,
-              position: 0
-            },
-            {
-              id: tablePlayers[1].id,
-              bet_amount: bet_amount,
-              position: 1
-            }
-          ]
-        }),
-        headers: { "Content-Type": "application/json" }
-      });
+  if (dealCardsData.ok) {
+    const dealCards = await dealCardsData.json();
+    dealer_hand_id = dealCards.tablePlayers[0].hand.id;
+    player_hand_id = dealCards.tablePlayers[1].hand.id;
+    dealer_cards = dealCards.tablePlayers[0].hand.cards.map(card => card);
+    player_cards = dealCards.tablePlayers[1].hand.cards.map(card => card);
 
-      if (dealCardsData.ok) {
-        const dealCards = await dealCardsData.json();
-        dealer_hand_id = dealCards.tablePlayers[0].hand.id;
-        player_hand_id = dealCards.tablePlayers[1].hand.id;
-        dealer_cards = dealCards.tablePlayers[0].hand.cards.map(card => card);
-        player_cards = dealCards.tablePlayers[1].hand.cards.map(card => card);
+    //remove placeholder cards
+    $('#dealer-hand').children().remove();
+    $('#player-hand').children().remove();
 
-        //remove placeholder cards
-        $('#dealer-hand').children().remove();
-        $('#player-hand').children().remove();
+    // display dealer cards
+    $('#dealer-hand').append(`<img class="dealerCard" src="../img/cards/BG.png" alt="Dealer_hideCard">`);
+    $('#dealer-hand').append(`<img class="dealerCard" src="../img/cards/${dealer_cards[1]}.png" alt="Dealer_hideCard">`);
 
-        // display dealer cards
-        $('#dealer-hand').append(`<img class="dealerCard" src="img/cards/BG.png" alt="Dealer_hideCard">`);
-        $('#dealer-hand').append(`<img class="dealerCard" src="img/cards/${dealer_cards[1]}.png" alt="Dealer_hideCard">`);
+    // display player cards
+    $('#player-hand').append(`<img class="dealerCard" src="../img/cards/${player_cards[0]}.png" alt="Dealer_hideCard">`);
+    $('#player-hand').append(`<img class="dealerCard" src="../img/cards/${player_cards[1]}.png" alt="Dealer_hideCard">`);
 
-        // display player cards
-        $('#player-hand').append(`<img class="dealerCard" src="img/cards/${player_cards[0]}.png" alt="Dealer_hideCard">`);
-        $('#player-hand').append(`<img class="dealerCard" src="img/cards/${player_cards[1]}.png" alt="Dealer_hideCard">`);
+    // enable hit and stand
+    $("#hit").attr('disabled', false);
+    $("#stand").attr('disabled', false);
+    $("#bet-more").attr('disabled', true);
+    $("#bet-less").attr('disabled', true);
 
-        // enable hit and stand
-        $("#hit").attr('disabled', false);
-        $("#stand").attr('disabled', false);
-        $("#bet-more").attr('disabled', true);
-        $("#bet-less").attr('disabled', true);
-
-      }
-    }
-    else {
-      // out of cards
-      const resetCards = await fetch('/api/gameRoutes/resetDeck/1', {
-        method: 'DELETE',
-        headers: { "Content-Type": "application/json" }
-      });
-      dealCards();
-    }
   }
-
 }
 
 const addCard = async (player_type) => {
@@ -142,7 +122,7 @@ const addCard = async (player_type) => {
     }
     else {
       dealer_cards.push(newCard.newCard);
-      $('#dealer-hand').append(`<img class="dealerCard" src="img/cards/${newCard.newCard}.png" alt="Dealer_hideCard">`);
+      $('#dealer-hand').append(`<img class="dealerCard" src="../img/cards/${newCard.newCard}.png" alt="Dealer_hideCard">`);
     }
 
     if (player_type === 'player') {
@@ -171,8 +151,8 @@ const playDealer = () => {
   $("#stand").attr('disabled', true);
   // reveal hidden card
   $('#dealer-hand').children().remove();
-  $('#dealer-hand').append(`<img class="dealerCard" src="img/cards/${dealer_cards[0]}.png" alt="Dealer_hideCard">`);
-  $('#dealer-hand').append(`<img class="dealerCard" src="img/cards/${dealer_cards[1]}.png" alt="Dealer_hideCard">`);
+  $('#dealer-hand').append(`<img class="dealerCard" src="../img/cards/${dealer_cards[0]}.png" alt="Dealer_hideCard">`);
+  $('#dealer-hand').append(`<img class="dealerCard" src="../img/cards/${dealer_cards[1]}.png" alt="Dealer_hideCard">`);
 
   // check card total
   const playerTotal = checkCardTotal('player');
@@ -289,6 +269,8 @@ const checkCardTotal = (player_type) => {
   return cardTotal;
 }
 
+const quitGame = async () => {
 
+}
 
 loadTable();
